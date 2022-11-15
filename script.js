7e4ef413b21a88c38ced1363f8ed112bff4851c0
Pavun786 <113338124+Pavun786@users.noreply.github.com>
2022-11-15 01:05:05 -0800

    day17 assignment

---
+++ script.js
@@ -0,0 +1,78 @@
+
+var res=document.getElementById("main")
+  var result=()=>{
+    setTimeout(()=>{
+        console.log(10)
+        res.innerText= 10
+    },1000)
+
+    setTimeout(()=>{
+        console.log(9)
+        res.innerText= 9
+    },2000)
+
+    setTimeout(()=>{
+        console.log(8)
+        res.innerText= 8
+    },3000)
+
+    setTimeout(()=>{
+        console.log(7)
+        res.innerText= 7
+    },4000)
+
+    setTimeout(()=>{
+        console.log(6)
+        res.innerText= 6
+    },5000)
+
+    setTimeout(()=>{
+        console.log(5)
+        res.innerText= 5
+    },6000)
+
+    setTimeout(()=>{
+        console.log(4)
+        res.innerText= 4
+    },7000)
+
+    setTimeout(()=>{
+        console.log(3)
+        res.innerText= 3
+    },8000)
+
+    setTimeout(()=>{
+        console.log(2)
+        res.innerText= 2
+    },9000)
+
+    setTimeout(()=>{
+        console.log(1)
+        res.innerText= 1
+    },10000)
+
+    setTimeout(()=>{
+        console.log("HAPPY INDEPENDENCE DAY")
+        
+        
+        res.innerHTML= `<h2>HAPPY INDEPENDENCE DAY</h2> `
+   },11000)
+   }
+    console.log(result())
+
+//  Method-2;
+
+    //   let countDown =(num)=>{
+       
+    //     if(num >= 1){
+    //     res.innerText= num
+     
+    //     setTimeout(()=>{
+    //     countDown(num - 1)
+    //  },1000)
+   
+    //  } else {
+    //     res.innerText="happy independence day"
+    //  }
+    //  } 
+    //    countDown(10)
